e4a75c6b7e2285a819953964d7d19d797124c433
45vikas <136835142+45vikas@users.noreply.github.com>
2023-06-17 11:24:25 +0530

    Add files via upload

---
+++ math library.py
@@ -0,0 +1,21 @@
+#math library
+#import math
+from math import *
+print(sqrt(8))
+print(pow(2,3))
+print(abs(-10))
+print(ceil(7.8))
+print(floor(7.8))
+print(pi)
+print(round(8.5978,2))
+#WAP  tp print area of triangle using heron's formula by taking all side of triangle
+#WAP  to calculateusing pow function
+
+a=int(input("entre the a side"))
+b=int(input("enter the b side"))
+c=int(input("entre the c side"))
+ s=print(a+b+c)/2
+print(sqrt(s*(s-a)*(s-b)*(s-c)))
+second=int(input("entre the second"))
+print(second/6)
+           
