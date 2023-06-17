37710e061c2e7cf30fd75104ac2b559408c99838
45vikas <136835142+45vikas@users.noreply.github.com>
2023-06-17 11:23:56 +0530

    Add files via upload

---
+++ vikas.py
@@ -0,0 +1,24 @@
+# arthematic operators
+ 
+a,b=4,5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
+# write a prograam to input and print area of circle
+# WAP to inpit three subject marks and calculate their average
+#and print it . also , print the percentage of marks
+r=int(input("enter the radius"))
+area=3.14*r*r
+print(area)
+# 2
+marks1=int(input("enter the first subjec num"))
+marks2=int(input("enter the second"))
+marks3=int(input("rnter the third"))
+avg=marks1+marks2+marks3/3
+print(avg);
+
+ 
