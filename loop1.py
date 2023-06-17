fa6f9cb8b77a3445ba1d95a90a6165fab41675d5
45vikas <136835142+45vikas@users.noreply.github.com>
2023-06-17 23:55:19 +0530

    Add files via upload

---
+++ loop1.py
@@ -0,0 +1,17 @@
+#code 1 on operation precending
+num=(5+3)*4*2/23
+print(num)
+# comparinon operators  { <,>,<=,>=,==,!=}
+print(3<5)
+print(4>2)
+print(2<=3)
+print(2==2)
+print(2!=3)
+# or operators any one is true than retuen the true value
+print(2>4 or 3>5)
+# and operators both conditon are true than return the true value
+print(2>1 and 4>2)
+# not operator is work that its makes the true to false and afalse to true
+print(not 2>1)
+print( not 3<1)
+
