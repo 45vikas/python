42b2a137ac0ec95acd505fde8de4d589173930d7
45vikas <136835142+45vikas@users.noreply.github.com>
2023-06-17 23:53:26 +0530

    Add files via upload

---
+++ ifelse2.py
@@ -0,0 +1,9 @@
+# if else statement in python
+age=14
+if(age>=23):
+    print("rohit sharma")
+elif(age>13 and age<18):
+    print("virat kholi")
+else:
+    print("surya")
+print("hloo rohit bhai")
